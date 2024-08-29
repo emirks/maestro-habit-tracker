96de1c29abab7a377c600ecdb049041a1ab44bf7
emirks <emirks88@gmail.com>
2024-08-29 13:04:41 +0300

    [docs] Add pokemon img urls

---
+++ tracking/__init__.py
@@ -0,0 +1,58 @@
+pokemon_urls = [
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png",   # Charizard
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png",   # Blastoise
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png",   # Venusaur
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/150.png", # Mewtwo
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/151.png", # Mew
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png",   # Ivysaur
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png",   # Charmeleon
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/8.png",   # Wartortle
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10.png",  # Caterpie
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/12.png",  # Butterfree
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/16.png",  # Pidgey
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png",  # Pikachu (again)
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/39.png",  # Jigglypuff
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/58.png",  # Growlithe
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/77.png",  # Ponyta
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/94.png",  # Gengar
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/130.png", # Gyarados
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/143.png", # Snorlax
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/149.png", # Dragonite
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/244.png", # Entei
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/248.png", # Tyranitar
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/251.png", # Celebi
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/373.png", # Salamence
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/384.png", # Rayquaza
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/445.png", # Garchomp
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/448.png", # Lucario
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/468.png", # Togekiss
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/470.png", # Leafeon
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/471.png", # Glaceon
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/475.png", # Gallade
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/479.png", # Rotom
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/484.png", # Palkia
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/493.png", # Arceus
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/643.png", # Reshiram
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/644.png", # Zekrom
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/649.png", # Genesect
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/722.png", # Rowlet
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/773.png", # Silvally
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/776.png", # Turtonator
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/809.png", # Melmetal
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/830.png", # Eldegoss
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/849.png", # Toxtricity
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/878.png", # Cufant
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/888.png", # Zacian
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/889.png", # Zamazenta
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/890.png", # Eternatus
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/898.png"  # Calyrex
+]
+
+
+dragon_urls = [
+    "https://www.dnd5eapi.co/api/images/monsters/aboleth.png",
+    "https://www.dnd5eapi.co/api/images/monsters/acolyte.png",
+    "https://www.dnd5eapi.co/api/images/monsters/adult-black-dragon.png",
+    "https://www.dnd5eapi.co/api/images/monsters/adult-blue-dragon.png",
+    "https://www.dnd5eapi.co/api/images/monsters/adult-brass-dragon.png"
+]
